206aa7b3d8e24601347252de4f25cc149ae36e2c
labankibet <laban.kibet@moringaschool.com>
2023-10-08 22:03:14 +0300

    done

---
+++ lib/migrations/versions/23cd5143c79d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 23cd5143c79d
+Revises: faebd12f9523
+Create Date: 2023-10-08 21:51:06.920281
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23cd5143c79d'
+down_revision = 'faebd12f9523'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,10 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, ForeignKey, Integer, String, MetaData, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+Session = sessionmaker(bind=engine)
+session = Session()
+engine = create_engine('sqlite:///one_to_many.db')
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,3 +12,22 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+Game.reviews = relationship('Review', backref=backref('game'))
